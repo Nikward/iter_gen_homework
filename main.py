4b816d4bc4e74d08fd3e9b22d4346b6068452d34
Nikolay Zolotuhin <nik.ward@yandex.ru>
2023-01-11 22:04:28 +0300

    delete excess

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-list_of_lists_1 = [
-        ['a', 'b', 'c'],
-        ['d', 'e', 'f', 'h', False],
-        [1, 2, None]
-    ]
 class FlatIterator:
 
     def __init__(self, super_list):
         self.general_list = super_list
 
     def __iter__(self):
-
         self.main_list_cursor = 0
         self.inner_list_cursor = -1
         self.current_iter = iter(self.general_list)
         return self
 
     def __next__(self):
-
         self.inner_list_cursor += 1
         if (self.inner_list_cursor) + 1 > len(self.general_list[self.main_list_cursor]):
             self.main_list_cursor += 1
@@ -26,7 +19,6 @@ class FlatIterator:
         return self.general_list[self.main_list_cursor][self.inner_list_cursor]
 
 def test_1():
-
     list_of_lists_1 = [
         ['a', 'b', 'c'],
         ['d', 'e', 'f', 'h', False],
